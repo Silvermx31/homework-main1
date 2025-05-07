dea4b04494cea1ed974e0ad9ac0490d22436f0c5
Silvermx31 <silvermx31@gmail.com>
2025-05-07 15:30:20 +0300

    fix db

--- database.php
+++ database.php
@@ -1,30 +1,21 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $user = 'root';
-    private $pass = '';
-    private $dbname = 'feedback_db';
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "feedbackdb";
     public $conn;
 
     public function __construct() {
         $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+
         if ($this->conn->connect_error) {
-            die('Ühendus ebaõnnestus: ' . $this->conn->connect_error);
+            die("Ühendus ebaõnnestus: " . $this->conn->connect_error);
         }
     }
 
     public function prepare($query) {
         return $this->conn->prepare($query);
-    }
-
-    public function query($query) {
-        return $this->conn->query($query);
-    }
-
-    public function deleteFeedback($id) {
-        $stmt = $this->prepare("DELETE FROM feedback WHERE id = ?");
-        $stmt->bind_param("i", $id);
-        return $stmt->execute();
     }
 }
 ?>
